2d277b67b3c870603ca430c43fc713d6d0d69173
Abbas Khan <ak6119231@gmail.com>
2024-04-06 17:04:29 +0500

    complete

--- src/components/PairExplorer/index.jsx
+++ src/components/PairExplorer/index.jsx
@@ -12,7 +12,7 @@ import UpcomingProjects from './UpcomingProjects'
 export default function Index() {
   return (
     <div className='relative z-50'>
-      {/* <div className='relative -z-0'>
+      <div className='relative -z-0 md:block hidden'>
         <Image
           src='/images/main-Ellipse.png'
           alt='sildebar-Ellipse'
@@ -20,7 +20,7 @@ export default function Index() {
           height={325}
           className='absolute right-0 top-0 w-[70%] h-[600px] translate-x-32  -translate-y-72  '
         />
-      </div> */}
+      </div>
       <TopSlide />
       <div className='grid grid-cols-12 my-10 md:gap-10 gap-0 '>
         <div className='md:col-span-8 col-span-12'>
