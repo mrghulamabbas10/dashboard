04ccc4f5bbfb670c4d01f880da427f0edae448a8
abbas khan <mrghulamabbas10@gmail.com>
2024-05-20 13:56:09 +0500

    remove-localstorage

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import LayoutForFillter from '@/components/layoutForFillter'
-import { data } from '@/components/home/config'
-
-export default function Home() {
-  return <LayoutForFillter initialData={data} />
-}
