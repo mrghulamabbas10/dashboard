817c3e415ffe2b4fdad6848ea51169ed9b1ec3b5
abbas khan <mrghulamabbas10@gmail.com>
2024-05-20 15:21:47 +0500

    ma

--- src/components/home/map.tsx
+++ src/components/home/map.tsx
@@ -10,10 +10,10 @@ export default function Map() {
       </div>
       <div className='mt-5'>
         <iframe
-          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52943468.08143988!2d-161.94000782076728!3d35.9123934915405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2s!4v1715997923325!5m2!1sen!2s'
+          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d462560.68283503666!2d54.8978108940377!3d25.076280444425517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f43496ad9c645%3A0xbde66e5084295162!2sDubai%20-%20United%20Arab%20Emirates!5e0!3m2!1sen!2s!4v1716200435775!5m2!1sen!2s'
           style={{ border: 0 }}
-          className='w-full md:h-[500px] h-[300px] object-cover rounded-3xl overflow-hidden'
           loading='lazy'
+          className='w-full md:h-[500px] h-[300px] object-cover rounded-3xl overflow-hidden'
         ></iframe>
       </div>
     </div>
